51d5292362178de6eea029405d7bc6b944b2f5fb
Mark <mark@askmark.io>
2024-06-15 00:19:02 -0500

    made some styling changes

--- main.py
+++ main.py
@@ -3,38 +3,34 @@ import click
 from rich.console import Console
 from rich.theme import Theme
 
-custom_theme = Theme({
-    'info': 'bold white',
-    'success': 'bold green',
-    'warning': 'bold yellow',
-    'danger': 'bold red'
-})
+custom_theme = Theme(
+    {
+        "info": "bold white",
+        "success": "bold green",
+        "warning": "bold yellow",
+        "danger": "bold red",
+    }
+)
 
 console = Console(theme=custom_theme)
+
 
 @click.group
 def mycommands():
     console.line()
-    console.rule("TODO", style='info')
+    console.rule("TODO", style="info")
     console.line()
 
 
-PRIORITIES = {
-    'o': 'Optional',
-    'l': 'Low',
-    'm': 'Medium',
-    'h': 'High',
-    'c': 'Critical'
-}
-
+PRIORITIES = {"o": "Optional", "l": "Low", "m": "Medium", "h": "High", "c": "Critical"}
 
 
 def user_input():
     console.line()
-    console.rule("TODO", style='info')
+    console.rule("TODO", style="info")
     console.line()
     description = """
-    Please choose an option:
+    [bold white]What would you like to do?
     
     1: Add TODO
     2: Delete TODO
@@ -45,9 +41,12 @@ def user_input():
     console.line()
 
     while True:
-        s = input("Please select the data you want: ")
+        console.print(
+            "Well, which one? ", style="info"
+        )
+        s = input().strip()
 
-        # Match to the user's input 
+        # Match to the user's input
         match s:
             case "1":
                 return add_todo()
@@ -59,63 +58,70 @@ def user_input():
                 exit_message()
                 return sys.exit()
             case _:
-                print("Please choose an option above.")
+                console.print("Please choose an option above.", style="warning")
 
 
 @click.command()
-@click.argument('priority', type=click.Choice(PRIORITIES.keys()), default='m')
-@click.argument('todofile', type=click.Path(exists=False), required=0)
-@click.option('-n', '--name', prompt='Enter the todo name', help='The name of the todo')
-@click.option('-d', '--desc', prompt='Describe the todo', help='The description of the todo')
-def add_todo(name, desc, priority, todofile):
-    filename = todofile if todofile is not None else 'TODO.md'
+@click.argument("priority", type=click.Choice(PRIORITIES.keys()), default="m")
+@click.argument("todofile", type=click.Path(exists=False), required=0)
+@click.option("-n", "--name", prompt="Enter the todo name", help="The name of the todo")
+def add_todo(name, priority, todofile):
+
+    # Add a direct prompt for description to avoid printing of brackets to prompt.
+    # desc = click.prompt(f'Describe the todo [bold yellow](press Enter to skip)[/]', default='', show_default=False)
+
+    console.print("[bold white]Describe the todo[/] (press Enter to skip)", end="")
+    desc = input().strip() or ""
+
+    filename = todofile if todofile is not None else "TODO.md"
     priority_val = PRIORITIES[priority]
-        
-    with open(filename, 'a+') as f:
+
+    with open(filename, "a+") as f:
         f.write(f"[ ] {name}: {desc} [Priority: {PRIORITIES[priority]}]")
-        f.write('\n')
+        f.write("\n")
         console.line()
-        console.rule('[bold green]Success[/]', style='success')
+        console.rule("[bold green]Success[/]", style="success")
         console.line()
-        console.print(f'Your TODO [bold green]{name}[/] with a priority of {priority_val} has been added!')
+        console.print(
+            f"Your TODO [bold green]{name}[/] with a priority of {priority_val} has been added!"
+        )
         console.line()
 
 
 @click.command()
-@click.argument('idx', type=int, required=1)
+@click.argument("idx", type=int, required=1)
 def delete_todo(idx):
-    with open('TODO', 'r') as f:
+    with open("TODO", "r") as f:
         todo_list = f.read().splitlines()
         todo_list.pop(idx)
-    with open('TODO', 'w') as f:
-        f.write('\n'.join(todo_list))
-        f.write('\n')
-
+    with open("TODO", "w") as f:
+        f.write("\n".join(todo_list))
+        f.write("\n")
 
 
 @click.command()
-@click.option('-p', '--priority', type=click.Choice(PRIORITIES.keys()))
-@click.argument('todofile', type=click.Path(exists=True), required=0)
+@click.option("-p", "--priority", type=click.Choice(PRIORITIES.keys()))
+@click.argument("todofile", type=click.Path(exists=True), required=0)
 def list_todos(priority, todofile):
-    filename = todofile if todofile is not None else 'TODO.md'
-    with open(filename, 'r') as f:
+    filename = todofile if todofile is not None else "TODO.md"
+    with open(filename, "r") as f:
         todo_list = f.read().splitlines()
     if priority is None:
         for idx, todo in enumerate(todo_list):
             print(f"({idx}) - {todo}")
 
+
 def exit_message():
     console.line()
-    console.print('You selected to exit!', style='warning')
+    console.print("You selected to exit!", style="warning")
     console.line()
-    console.rule('Goodbye', style='warning')
+    console.rule("Goodbye", style="warning")
     console.line()
 
 
 mycommands.add_command(add_todo)
 mycommands.add_command(delete_todo)
 mycommands.add_command(list_todos)
-
 
 
 if __name__ == "__main__":
